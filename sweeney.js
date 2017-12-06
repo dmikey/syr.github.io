b1341a56563bac67597af0ef22b6b309c056f57c
Gabriel Csapo <gabecsapo@gmail.com>
2017-12-05 23:26:56 -0800

    initial design

---
+++ sweeney.js
@@ -0,0 +1,13 @@
+module.exports = {
+  source: './',
+  output: './docs',
+  site: {
+    main: './index.html',
+    title: 'Syr',
+    description: 'Minimal react compatible API, aimed at native api developers.',
+    pages: [{
+      title: 'Source',
+      url: 'http://github.com/gabrielcsapo/sweeney'
+    }]
+  }
+};
